c231cd034f08c2caf9d15cc697f2741aad877a10
De-Lit@users <De-Lit@users.noreply.github.com>
2022-02-23 20:45:32 +0700

    Initial repository

---
+++ src/ServerWPF/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerWPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow() 
+        {
+            try
+            {
+                Server server = new Server(80);
+            }
+            catch
+            {
+
+            }
+            InitializeComponent();
+
+            Button.Click += SendMesage;
+        }
+        private void SendMesage(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+                IPAddress broadcast = IPAddress.Parse(ipAdress.Text);
+
+                byte[] sendbuf = Encoding.ASCII.GetBytes("1");
+                IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+                s.SendTo(sendbuf, ep);
+
+                Console.WriteLine("Message sent to the broadcast address");
+                System.Diagnostics.Process.Start(@"c:\Games\localhost\WebApplication2.exe");
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
+
---
+++ src/ServerWPF/Server.cs
@@ -0,0 +1,46 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ServerWPF
+{
+    class Server
+    {
+        TcpListener Listener; // Объект, принимающий TCP-клиентов
+
+        // Запуск сервера
+        public Server(int Port)
+        {
+            // Создаем "слушателя" для указанного порта
+
+
+            Listener = new TcpListener(IPAddress.Any, Port);
+            Listener.Start(); // Запускаем его
+            Method();
+
+            // В бесконечном цикле
+            async void Method()
+            {
+                await Task.Run(() =>
+                {
+                    while (true)
+                    {
+                        // Принимаем новых клиентов и передаем их на обработку новому экземпляру класса Client
+                        new Client(Listener.AcceptTcpClient());
+                    }
+                });
+            }
+        }
+
+        // Остановка сервера
+        ~Server()
+        {
+            // Если "слушатель" был создан
+            if (Listener != null)
+            {
+                // Остановим его
+                Listener.Stop();
+            }
+        }
+    }    
+}
